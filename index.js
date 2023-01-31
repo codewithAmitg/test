7e0e51e64cb29883013c6f897caa8e79af28bea7
codewithamitg <codewithamitgupta@gmail.com>
2023-01-31 23:56:32 +0545

    test

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 9000;
 app.use("/", (req, res) => {
   res.json({ message: "Hello From Express App" });
 });
+app.use("/test", (req, res) => {
+  res.json({ message: "Hello From Express test" });
+});
 
 app.listen(9000, () => {
   console.log(`Starting Server on Port ${port}`);
